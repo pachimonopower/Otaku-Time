cfbd7a618c953b7a05327118bd933f6d3e7d1369
Jack Ryder <me@jackryder.co.uk>
2017-08-05 15:35:31 +0100

    Added some basic tests.

---
+++ OtakuTimeTests/OtakuTimeMainTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Otaku_Time;
+
+namespace OtakuTimeTests
+{
+    [TestClass]
+    public class OtakuTimeMainTests
+    {
+        [TestMethod]
+        public void GetPhantomObjectTest()
+        {
+            var PhantomObject = Statics.PhantomObject();
+            Assert.AreNotEqual(null, PhantomObject);
+        }
+
+        [TestMethod]
+        public void ConnectToMasterTest()
+        {
+            var PhantomObject = Statics.PhantomObject();
+            PhantomObject.Navigate().GoToUrl($"http://{Statics.MasterURL}/M");
+            System.Threading.Thread.Sleep(6000); //bypass cloudflare
+            string title = PhantomObject.Title;
+            Assert.IsTrue(title.ToLower().Contains("watch"));
+        }
+    }
+}
